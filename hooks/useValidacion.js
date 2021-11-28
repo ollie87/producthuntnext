5b46e6690b483ce5c0a036e8ecd6c4141415bdbd
mgarridov <mgarridov@minsait.com>
2021-11-28 19:23:52 +0100

    feat(useValidation): hook added

---
+++ hooks/useValidacion.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+
+const useValidacion = (stateInicial, validar, fn) => {
+    const [valores, guardarValores] = useState(stateInicial);
+    const [errores, guardarErrores] = useState({});
+    const [submitForm, guardarSubmitForm] = useState(false);
+
+    useEffect(() => {
+        if(submitForm) {
+            const noErrores = Object.keys(errores).length === 0;
+            if (noErrores) {
+                fn(); // Fn = Función que se ejecuta en el componente
+            }
+            guardarSubmitForm(false)
+        }
+    }, [])
+
+    // Función que se ejecuta conforme el usuario escribe algo
+    const handleChange = e => {
+        guardarValores({
+            ...valores,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // Función que se ejecuta cuando el usuario hace submit
+    const handleSubmit = e => {
+        e.preventDefault();
+        const erroresValidacion = validar;
+        guardarErrores(erroresValidacion);
+        guardarSubmitForm(true)
+    }
+    return {
+        valores,
+        errores,
+        submitForm,
+        handleSubmit,
+        handleChange
+    };
+}
+
+export default useValidacion;
